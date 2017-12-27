f8c79b2e8d1c20e8d5ec59d7ae9356041dc7ccd4
Jay Martinez <jay.martinez7@yahoo.com>
2017-12-27 17:37:23 -0600

    java activities

---
+++ test/hello.java
@@ -0,0 +1,11 @@
+package test;
+
+public class hello{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
+
+//in terminal to compile this, you just type 
+//$ java -cp .. test.hello
+//and it will print Hello World!
